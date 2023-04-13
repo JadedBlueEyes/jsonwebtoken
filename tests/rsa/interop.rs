3ccab64340a79f91244c4fe707ccb495b8a86b46
Jade Ellis <jade@ellis.link>
2023-04-13 13:31:31 +0100

    Format & fix.

--- tests/rsa/interop.rs
+++ tests/rsa/interop.rs
@@ -13,13 +13,21 @@ fn round_trip_sign_verification_pem_pkcs1_jsonwebtoken_to_crate() {
     let pubkey: rsa::RsaPublicKey =
         rsa::pkcs1::DecodeRsaPublicKey::from_pkcs1_pem(include_str!("public_rsa_key_pkcs1.pem"))
             .unwrap();
-    
+
     for (alg_rc, alg_ring) in RSA_ALGORITHMS {
-        let encrypted =
-            jsonwebtoken::crypto::sign("hello world".as_bytes(), &jsonwebtoken::EncodingKey::from_rsa_pem(privkey_pem).unwrap(), *alg_ring).unwrap();
-        let is_valid =
-            jsonwebtoken_rustcrypto::crypto::verify(&encrypted, "hello world", &jsonwebtoken_rustcrypto::DecodingKey::from_rsa(pubkey.clone()).unwrap(), *alg_rc)
-                .unwrap();
+        let encrypted = jsonwebtoken::crypto::sign(
+            "hello world".as_bytes(),
+            &jsonwebtoken::EncodingKey::from_rsa_pem(privkey_pem).unwrap(),
+            *alg_ring,
+        )
+        .unwrap();
+        let is_valid = jsonwebtoken_rustcrypto::crypto::verify(
+            &encrypted,
+            "hello world",
+            &jsonwebtoken_rustcrypto::DecodingKey::from_rsa(pubkey.clone()).unwrap(),
+            *alg_rc,
+        )
+        .unwrap();
         assert!(is_valid, "{:?} signature was not valid.", alg_rc);
     }
 }
@@ -30,13 +38,21 @@ fn round_trip_sign_verification_pem_pkcs1_crate_to_jsonwebtoken() {
     let privkey: rsa::RsaPrivateKey =
         rsa::pkcs1::DecodeRsaPrivateKey::from_pkcs1_pem(include_str!("private_rsa_key_pkcs1.pem"))
             .unwrap();
-    
+
     for (alg_rc, alg_ring) in RSA_ALGORITHMS {
-        let encrypted =
-            jsonwebtoken_rustcrypto::crypto::sign("hello world", &jsonwebtoken_rustcrypto::EncodingKey::from_rsa(privkey.clone()).unwrap(), *alg_rc).unwrap();
-        let is_valid =
-            jsonwebtoken::crypto::verify(&encrypted, "hello world".as_bytes(), &jsonwebtoken::DecodingKey::from_rsa_pem(pubkey_pem).unwrap(), *alg_ring)
-                .unwrap();
+        let encrypted = jsonwebtoken_rustcrypto::crypto::sign(
+            "hello world",
+            &jsonwebtoken_rustcrypto::EncodingKey::from_rsa(privkey.clone()).unwrap(),
+            *alg_rc,
+        )
+        .unwrap();
+        let is_valid = jsonwebtoken::crypto::verify(
+            &encrypted,
+            "hello world".as_bytes(),
+            &jsonwebtoken::DecodingKey::from_rsa_pem(pubkey_pem).unwrap(),
+            *alg_ring,
+        )
+        .unwrap();
         assert!(is_valid, "{:?} signature was not valid.", alg_rc);
     }
 }
